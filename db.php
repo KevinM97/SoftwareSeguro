5e0b26f6812f461c19bb40ab64b44e69808ef4ee
KevinM97 <khmina@espe.edu.ec>
2022-06-14 11:04:19 -0500

    Cadena de conexion e index con php

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect(
+    'bivrslvhvcnbmgomi3to-mysql.services.clever-cloud.com',
+    'ux3s6yt0aynviuls',
+    'vs01EGwzvtpAcqVPw98j',
+    'bivrslvhvcnbmgomi3to'
+);
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Grupo 4"
+
+?>
