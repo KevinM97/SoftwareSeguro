49d2879bbae572ca619cea15ab20f1265924095c
Soporte1 <soporte1@iconta.ec>
2022-06-16 11:18:38 -0500

    Pagina de Campus-Diseño

--- home.php
+++ home.php
@@ -6,44 +6,111 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+
+    <!-- Custom fonts for this template-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
     <title>HOME CAMPUS</title>
 </head>
 
 <body>
-    <div class="cover-container bg-dark d-flex h-100 p-3 mx-auto flex-column">
-        <div class=" text-white text-center m-4">
-            <h1>{CAMPUS}</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Desarrollo Seguro</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="campus.php">Campus <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bar</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Menu
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Preferencias</a>
+
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Snack</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Buzón</a>
+                </li>
+            </ul>
+
         </div>
-        <div class="d-flex justify-content-around">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="https://img.freepik.com/vector-gratis/elementos-comida-dibujados-mano_1411-48.jpg?t=st=1655388224~exp=1655388824~hmac=5d472b1ca7a39c71e8a38ef6643192a471b4eb05515e08960bffa294e9af5d4f&w=900" alt="Bar1">
-                <div class="card-body">
-                    <p class="card-text text-center">Bar 1</p>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="#" class="card-link">Ir</a>
-                </div>
+    </nav>
+    <div class="d-flex p-3 justify-content-around">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Campus</h5>
+
+                <a href="campus.php" class="btn btn-primary">Ir</a>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="https://img.freepik.com/vector-gratis/elementos-comida-dibujados-mano_1411-48.jpg?t=st=1655388224~exp=1655388824~hmac=5d472b1ca7a39c71e8a38ef6643192a471b4eb05515e08960bffa294e9af5d4f&w=900" alt="Bar2">
-                <div class="card-body">
-                    <p class="card-text text-center">Bar 2</p>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="#" class="card-link">Ir</a>
-                </div>
+
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Bar</h5>
+
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Snack</h5>
+
+                <a href="#" class="btn btn-primary">Ir</a>
             </div>
         </div>
-        <div class="m-4 text-center  text-white" style="height: 50px;">
-            <p>DESARROLLO DE SOFTWARE SEGURO</p>
-            
+       
+    </div>
+    <div class="d-flex  p-3 justify-content-around">
+        <div class="card " style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body  text-center">
+                <h5 class="card-title">Buzon</h5>
+
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+
         </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Menu</h5>
+
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://img.freepik.com/vector-gratis/supermercado-alimentos-estantes-berenjena-repollo-zanahoria-pimientos-cebollas-pan-maiz-patatas-compras-frescas-ilustracion-vectorial_1284-46271.jpg?t=st=1655395498~exp=1655396098~hmac=f458546ec7f0bf501b5b03ecc63ecb563577ed9f49cf8e0c7e9d52e1905a7d6f&w=900" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Preferencias</h5>
+
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+       
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
